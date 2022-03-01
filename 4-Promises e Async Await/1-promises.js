c10e83ffdcd93ffc166d4023b9d71e332efa9e05
phap9060 <phap9060@gmail.com>
2022-02-28 21:20:36 -0300

    refatorando async await

--- 4-Promises e Async Await/1-promises.js
+++ 4-Promises e Async Await/1-promises.js
@@ -1,61 +1,53 @@
-const desafio1asRosasNaoFalam =  () => {
+const desafio1asRosasNaoFalam = () => {
+  const geraIntervaloAleatório = () => Math.floor(Math.random() * 3 * 1000);
 
-    const geraIntervaloAleatório = () => Math.floor(Math.random() * 3 * 1000);
-    
-    // métodos de versos. Estes métodos deverão ser modificados.
-    const queixoMeAsRosas = () => {
-    return new Promise(res =>{
-        setTimeout(() => {
-            console.log('Queixo-me às rosas');
-            res()
-        }, geraIntervaloAleatório());
-    })};
-  
-    const masQueBobagem = () => {
-        return new Promise(res => {
-            setTimeout(() => {
-                console.log('Mas que bobagem');
-                res()
-            }, geraIntervaloAleatório())
+  // métodos de versos. Estes métodos deverão ser modificados.
+  const queixoMeAsRosas = () => {
+    return new Promise((res) => {
+      setTimeout(() => {
+        console.log("Queixo-me às rosas");
+        res();
+      }, geraIntervaloAleatório());
+    });
+  };
 
-        })};
-  
-    const asRosasNaoFalam = () => {
-     return new Promise(res =>{
-        setTimeout(() => {
-            console.log('As rosas não falam');
-            res()
-          }, geraIntervaloAleatório());
-     })
-     
-    };
-  
-    const simplesmenteAsRosasExalam = () => {
-     return new Promise(res => {
-        setTimeout(() => {
-            console.log('Simplesmente as rosas exalam');
-            res()
-          }, geraIntervaloAleatório());
-     })   
-     
-    };
-  
-    const oPerfumeQueRoubamDeTi = () => {
-       return new Promise(res =>{
-        setTimeout(() => {
-            console.log('O perfume que roubam de ti, ai');
-            res()
-        }, geraIntervaloAleatório());
-       }) 
-      
-    };
+  const masQueBobagem = () => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res(console.log("Mas que bobagem"));
+      }, geraIntervaloAleatório());
+    });
+  };
 
-   queixoMeAsRosas()
-   .then(()=> masQueBobagem())
-   .then(()=> asRosasNaoFalam())
-   .then(()=> simplesmenteAsRosasExalam())
-   .then(()=> oPerfumeQueRoubamDeTi())
-  
-}
+  const asRosasNaoFalam = () => {
+    return new Promise((res) => {
+      setTimeout(() => {
+        res(console.log("As rosas não falam"));
+      }, geraIntervaloAleatório());
+    });
+  };
 
-desafio1asRosasNaoFalam()
+  const simplesmenteAsRosasExalam = () => {
+    return new Promise((res) => {
+      setTimeout(() => {
+        res(console.log("Simplesmente as rosas exalam"));
+      }, geraIntervaloAleatório());
+    });
+  };
+
+  const oPerfumeQueRoubamDeTi = () => {
+    return new Promise((res) => {
+      setTimeout(() => {
+        res(console.log("O perfume que roubam de ti, ai"));
+      }, geraIntervaloAleatório());
+    });
+  };
+
+  queixoMeAsRosas()
+    .then(() => masQueBobagem())
+    .then(() => asRosasNaoFalam())
+    .then(() => simplesmenteAsRosasExalam())
+    .then(() => oPerfumeQueRoubamDeTi());
+};
+
+desafio1asRosasNaoFalam();
--- 4-Promises e Async Await/2-async.js
+++ 4-Promises e Async Await/2-async.js
@@ -1,54 +1,48 @@
 const desafio1asRosasNaoFalam = async () => {
 
     const geraIntervaloAleatório = () => Math.floor(Math.random() * 3 * 1000);
-    
-    
+
+    // métodos de versos. Estes métodos deverão ser modificados.
     const queixoMeAsRosas = () => {
-        return new Promise(res =>{
-            setTimeout(() => {
-                console.log('Queixo-me às rosas');
-                res()
-            }, geraIntervaloAleatório());
-        })};
-      
-        const masQueBobagem = () => {
-            return new Promise(res => {
-                setTimeout(() => {
-                    console.log('Mas que bobagem');
-                    res()
-                }, geraIntervaloAleatório())
-    
-            })};
-      
-        const asRosasNaoFalam = () => {
-         return new Promise(res =>{
-            setTimeout(() => {
-                console.log('As rosas não falam');
-                res()
-              }, geraIntervaloAleatório());
-         })
-         
-        };
-      
-        const simplesmenteAsRosasExalam = () => {
-         return new Promise(res => {
-            setTimeout(() => {
-                console.log('Simplesmente as rosas exalam');
-                res()
-              }, geraIntervaloAleatório());
-         })   
-         
-        };
-      
-        const oPerfumeQueRoubamDeTi = () => {
-           return new Promise(res =>{
-            setTimeout(() => {
-                console.log('O perfume que roubam de ti, ai');
-                res()
-            }, geraIntervaloAleatório());
-           }) 
-          
-        };
+      return new Promise((res) => {
+        setTimeout(() => {
+          console.log("Queixo-me às rosas");
+          res();
+        }, geraIntervaloAleatório());
+      });
+    };
+  
+    const masQueBobagem = () => {
+      return new Promise((res, rej) => {
+        setTimeout(() => {
+          res(console.log("Mas que bobagem"));
+        }, geraIntervaloAleatório());
+      });
+    };
+  
+    const asRosasNaoFalam = () => {
+      return new Promise((res) => {
+        setTimeout(() => {
+          res(console.log("As rosas não falam"));
+        }, geraIntervaloAleatório());
+      });
+    };
+  
+    const simplesmenteAsRosasExalam = () => {
+      return new Promise((res) => {
+        setTimeout(() => {
+          res(console.log("Simplesmente as rosas exalam"));
+        }, geraIntervaloAleatório());
+      });
+    };
+  
+    const oPerfumeQueRoubamDeTi = () => {
+      return new Promise((res) => {
+        setTimeout(() => {
+          res(console.log("O perfume que roubam de ti, ai"));
+        }, geraIntervaloAleatório());
+      });
+    };
   
     await queixoMeAsRosas()
     await masQueBobagem()
